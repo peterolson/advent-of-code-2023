a384cb2da6fca9d95be0c4d00312cf4b668e170c
Peter Olson <peter.e.c.olson+github@gmail.com>
2023-12-07 00:43:00 -0600

    got it

--- 7.py
+++ 7.py
@@ -9,68 +9,43 @@ card_values = {
     "T": 10, "J": 11, "Q": 12, "K": 13, "A": 14
 }
 
-def score_hand(hand : str):
-    values = [card_values[card] for card in hand]
-    counts = [values.count(card_value) for card_value in set(values)]
-
-    # five of a kind
-    if 5 in counts:
-        return 9, values
-    # four of a kind
-    if 4 in counts:
-        return 8, values
-    # full house
-    if 3 in counts and 2 in counts:
-        return 7, values
-    # three of a kind
-    if 3 in counts:
-        return 6, values
-    # two pair
-    if counts.count(2) == 2:
-        return 5, values
-    # pair
-    if 2 in counts:
-        return 4, values
-    # high card
-    return 0, values
-
 hands = [line.split()[0] for line in lines]
 bids = [int(line.split()[1]) for line in lines]
 
-bid_scores = [(score_hand(hand), bid) for hand, bid in zip(hands, bids)]
-
 def compare_bid_score(a, b):
-    a_score, a_bid = a
-    b_score, b_bid = b
-    a_type, a_values = a_score
-    b_type, b_values = b_score
-    if a_type > b_type:
-        return 1
-    if a_type < b_type:
-        return -1
-    for a_value, b_value in zip(a_values, b_values):
-        if a_value > b_value:
+    a_hand, a_bid = a
+    b_hand, b_bid = b
+    counts_a = sorted([a_hand.count(card) for card in set(a_hand)], reverse=True)
+    counts_b = sorted([b_hand.count(card) for card in set(b_hand)], reverse=True)
+    for count_a, count_b in zip(counts_a, counts_b):
+        if count_a > count_b:
             return 1
-        if a_value < b_value:
+        if count_a < count_b:
+            return -1
+    for a_value, b_value in zip(a_hand, b_hand):
+        if card_values[a_value] > card_values[b_value]:
+            return 1
+        if card_values[a_value] < card_values[b_value]:
             return -1
     return 0
 
-sorted_bid_scores = sorted(bid_scores, key=cmp_to_key(compare_bid_score))
-winnings = [((i + 1), bid_score[1]) for i, bid_score in enumerate(sorted_bid_scores)]
+hand_bids = [(hand, bid) for hand, bid in zip(hands, bids)]
+sorted_hand_bids = sorted(hand_bids, key=cmp_to_key(compare_bid_score))
+winnings = [((i + 1), bid_score[1]) for i, bid_score in enumerate(sorted_hand_bids)]
 total_winnings = sum([a * b for a, b in winnings])
 
-print(total_winnings)
+print(f"Part 1: {total_winnings}")
 
 # part 2
 
 card_values["J"] = 0
 
 def convert_jokers(hand):
-    counts = [hand.count(card) for card in hand]
-    max_count = max(counts)
-    cards_with_max_count = [card for card, count in zip(hand, counts) if count == max_count]
-    highest_card = sorted(cards_with_max_count, key=lambda card: card_values[card])[-1]
-    return hand.replace("J", highest_card)
+    non_jokers = hand.replace("J", "")
+    if len(non_jokers) == 0:
+        return hand
+    most_common_card = max(set(non_jokers), key=hand.count)
+    return hand.replace("J", most_common_card)
 
 hands = [line.split()[0] for line in lines]
 bids = [int(line.split()[1]) for line in lines]
@@ -80,27 +55,26 @@ def compare_hands(a, b):
     b_cards, b_bid = b
     a_converted = convert_jokers(a_cards)
     b_converted = convert_jokers(b_cards)
-    a_score, _ = score_hand(a_converted)
-    b_score, _ = score_hand(b_converted)
-    if a_score > b_score:
-        return 1
-    if a_score < b_score:
-        return -1
-    a_values = [card_values[card] for card in a_cards]
-    b_values = [card_values[card] for card in b_cards]
-    for a_value, b_value in zip(a_values, b_values):
-        if a_value > b_value:
+    
+    counts_a = sorted([a_converted.count(card) for card in set(a_converted)], reverse=True)
+    counts_b = sorted([b_converted.count(card) for card in set(b_converted)], reverse=True)
+    for count_a, count_b in zip(counts_a, counts_b):
+        if count_a > count_b:
             return 1
-        if a_value < b_value:
+        if count_a < count_b:
+            return -1
+    
+    for a_card, b_card in zip(a_cards, b_cards):
+        if card_values[a_card] > card_values[b_card]:
+            return 1
+        if card_values[a_card] < card_values[b_card]:
             return -1
     return 0
 
 hand_bids = [(hand, bid) for hand, bid in zip(hands, bids)]
 sorted_hand_bids = sorted(hand_bids, key=cmp_to_key(compare_hands))
 
-print(sorted_hand_bids)
-
 winnings = [((i + 1), bid_score[1]) for i, bid_score in enumerate(sorted_hand_bids)]
 total_winnings = sum([a * b for a, b in winnings])
 
-print(total_winnings)
+print(f"Part 2: {total_winnings}")
