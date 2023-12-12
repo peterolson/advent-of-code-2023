6f91e967c527b16e973ed358604ed7b3b63074be
Peter Olson <peter.e.c.olson+github@gmail.com>
2023-12-12 11:28:32 -0600

    ok complicated but works

--- 12.py
+++ 12.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 with open("input/12.txt", "r") as f:
     lines = f.readlines()
 
@@ -9,38 +11,97 @@ for line in lines:
     groups = [int(group) for group in groups.split(",")]
     records.append((springs, groups))
 
-def get_chunk_arrangements(chunk : str, groups : list[int], group_start : int, dict : dict[int, int]):
-    if len(chunk) == 0:
-        print("end", group_start)
-        dict[group_start] = dict.get(group_start, 0) + 1
-        return
-    next_group = groups[0]
-    i = 0
-    while i + next_group <= len(chunk):
-        is_valid = i + next_group == len(chunk) or chunk[i + next_group] == "?"
-        print(chunk, i, next_group, is_valid)
-        if is_valid:
-            get_chunk_arrangements(chunk[i + next_group + 1:], groups, group_start + 1, dict)
-        i += 1
-    dict[group_start] = dict.get(group_start, 0) + 1
-            
-def get_arrangements(record):
-    springs, groups = record
-    chunks = [chunk for chunk in springs.split(".") if len(chunk) > 0]
-    queue = [(chunks[0], 0, {}, 1)]
-    grand_total = 0
-    while len(queue) > 0:
-        print(queue)
-        chunk, i, dict, total = queue.pop(0)
-        get_chunk_arrangements(chunk, groups, i, dict)
-        print(dict)
-        for group_start, count in dict.items():
-            if group_start == len(groups) and i == len(chunks) - 1:
-                grand_total += total * count
-            elif i < len(chunks) - 1:
-                queue.append((chunks[i + 1], group_start, {}, total * count))
 
-get_arrangements(records[0])
+def get_arrangements(springs, groups, depth=0):
+    prefix = "    " * depth
+    # print(prefix, springs, groups)
+    if len(groups) == 0:
+        if "#" in springs:
+            return 0
+        # print(prefix + "yay!")
+        return 1
+    broken = springs.count("#")
+    remaining_sum = sum(groups)
+    if broken > remaining_sum:
+        return 0
+    unknown = springs.count("?")
+    if broken + unknown < remaining_sum:
+        return 0
+
+    next_group = groups[0]
+    groups_tail = groups[1:]
+    min_tail_length = sum(groups_tail) + len(groups_tail)
+    region_to_fill = springs[:-min_tail_length] if len(groups_tail) > 0 else springs
+    original_region_to_fill = region_to_fill
+    while (
+        min_tail_length > 0
+        and springs[-min_tail_length] == "#"
+        and region_to_fill[-1] == "#"
+    ):
+        region_to_fill += "#"
+        min_tail_length -= 1
+
+    i = 0
+    total_count = 0
+    while i + next_group <= len(original_region_to_fill):
+        chunk = region_to_fill[i : i + next_group]
+        # print(prefix, region_to_fill, chunk, i)
+        if "." in chunk:
+            i += 1
+            continue
+        if i + next_group < len(springs) and springs[i + next_group] == "#":
+            i += 1
+            continue
+        if "#" in region_to_fill[0:i]:
+            break
+        total_count += get_arrangements(
+            springs[i + next_group + 1 :], groups_tail, depth + 1
+        )
+        if chunk[0] == "#":
+            break
+        i += 1
+    return total_count
+
+
+def get_arrangements_2(record):
+    springs, groups = record
+    total_damaged = sum(groups)
+    unknown = springs.count("?")
+    known_damaged = springs.count("#")
+    missing_damaged = total_damaged - known_damaged
+    arrangements = combinations(range(unknown), missing_damaged)
+    valid_arrangements = 0
+    for combination in arrangements:
+        # replace ? with combination
+        arrangement = ""
+        i = 0
+        for spring in springs:
+            if spring == "?":
+                if i in combination:
+                    arrangement += "#"
+                else:
+                    arrangement += "."
+                i += 1
+            else:
+                arrangement += spring
+        arrangement_groups = [
+            len(group) for group in arrangement.replace(".", " ").strip().split()
+        ]
+        if arrangement_groups == groups:
+            valid_arrangements += 1
+    return valid_arrangements
+
+
+total_1 = 0
+total_2 = 0
+for record in records:
+    count_1 = get_arrangements(*record)
+    count_2 = get_arrangements_2(record)
+    if count_1 != count_2:
+        print(record, count_1, count_2)
+    total_1 += count_1
+    total_2 += count_2
+print(total_1, total_2)
 
 # arrangement_counts = []
 # for i, record in enumerate(records):
@@ -61,6 +122,6 @@ get_arrangements(records[0])
 #     ratio_2 = triple_counts / counts
 #     total_2 = counts * ratio_2 ** 2
 #     print(i, counts, double_counts, triple_counts, ratio, ratio_2, total, total_2)
-        
+
 #     # 0 23570904
 # print(sum(arrangement_counts))
